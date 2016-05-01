77befa76e11f3013a9d0a1bc1ba061efa6f19181
joshring <frag180@gmail.com>
2016-05-01 19:13:00 +0100

    Update pyfftw_test.py

--- pyfftw_test.py
+++ pyfftw_test.py
@@ -1,59 +1,93 @@
 """
-This is a FFT and numba profiling test file
+This is a FFT profiling test file
 FFTW library is 3.56x faster than numpy's fft
-Numba is 31x faster than stock python when doing loops over arrays
-
-Numba is 92.8% as fast as numpy when doing simple array modifications,
-but being able to change per iteration is a big advantage, 
-e.g. conditional modification, not possible in numpy
 
 by Josh Ring
-29/04/16
-
+01/05/16
 """
 import time # For timing processes
 import numpy as np  # Array library more flexible than default
 import pyfftw   # FFTW library python bindings
 import multiprocessing # Gets num cpu threads
 import matplotlib.pyplot as plt # graph plotting
-nthreads = multiprocessing.cpu_count()
 
 nthreads = multiprocessing.cpu_count()
-sampleRate = 128 # Hz
-tInput = 100000
+sampleRate = 41000 # Hz
+tInput = 10 # Seconds
 lenInput = tInput * sampleRate
-freq1 = 20 # Hz  
-freq2 = 50 # Hz
+freq1 = 200 # Hz  
+freq2 = 7000 # Hz
 nyquist = 2*sampleRate # Hz, this is the max freq, can reliably be sampled
 
-start = time.time() # start timer
-# Numpy data initialisation
-time_s = np.linspace( 0.0, (lenInput/sampleRate),  num = lenInput )
-input = np.random.rand(lenInput).astype('complex128') # 64 bit real and 64 bit imag
-input += np.cos( 2*np.pi*time_s * freq1 ) + np.cos( 2*np.pi*time_s * freq2 )
+def dataInitialise(lenInput, sampleRate):
+    """
+    Creates linear sequences for time to create cosines of known frequencies
+    Creates noise to add to the signal
+    Returns fft test input data as a numpy array
+    """
+    start = time.time() # start timer
+    time_s = np.linspace( 0.0, (lenInput/sampleRate),  num = lenInput )
+    inputData = 1*np.random.rand(lenInput).astype('complex128') # 64 bit real and 64 bit imag, further 4x speed if use 32bit
+    inputData += np.cos( 2*np.pi*time_s * freq1 ) + np.cos( 2*np.pi*time_s * freq2 )
+    
+    timetaken = time.time() - start
+    print('Time taken for data initialisation %.3f secs.' % (timetaken))
+    return inputData 
 
-timetaken = time.time() - start
-print('Time taken for data initialisation %.3f secs.' % (timetaken))
 
-pyfftw.forget_wisdom() # Detail of FFTW
+def fft( inputData ):
+    """
+    Takes inputData as numpy array
+    Returns forward FFT calculated by FFTW
+    """
+    pyfftw.forget_wisdom() # Detail of FFTW
+    start = time.time()
+    fftOutput = np.empty_like(inputData)
+    # Setting up and running FFT (forward) calculation
+    fft = pyfftw.FFTW( inputData, fftOutput, direction='FFTW_FORWARD', flags=('FFTW_MEASURE', ), threads=nthreads, planning_timelimit=None )
+    fft()
+    
+    timetaken = time.time() - start
+    print('Time taken for FFT with %d cores is %.3f secs.' % (nthreads, timetaken))
+    fftOutput = (1/ (0.5*len(fftOutput)) ) * fftOutput # FFT normalisation
+    return fftOutput
 
-start = time.time()
-fftout = np.zeros_like(input)
-# Setting up and running FFT (forward) calculation
-fft = pyfftw.FFTW( input, fftout, direction='FFTW_FORWARD', flags=('FFTW_MEASURE', ), threads=nthreads, planning_timelimit=None )
-fft()
-timetaken = time.time() - start
-print('Time taken for FFT with %d cores is %.3f secs.' % (nthreads, timetaken))
 
-# Frequencies from 0 to 1/2 sampling freq, number of elements = 1/2 total, due to FFT symmetry
-freq = np.linspace(0.0, (0.5*sampleRate), (lenInput*0.5))   
-fftout = (1/ (0.5*lenInput)) * fftout # FFT normalisation
+def plotGraphs( fftOutput, sampleRate ):
+    """
+    Use matplotlib to plot graphs
+    
+    TODO: Not currently set up to handle odd numbered lengths of input
+    """
+    lenInput = int( len(fftOutput) )
+    halfLenInput = int( lenInput/2 ) # Used for graphing 
 
-""" Plot frequencies in signal """
-plt.plot( freq, np.abs( fftout[:(0.5*lenInput)] ) ) # Take first half of FFT, take abs value
-plt.grid()
-plt.show()
+    # Frequencies from 0 to 1/2 sampling freq, number of elements = 1/2 total, due to FFT symmetry
+    freq = np.linspace( 0.0, (0.5*sampleRate), (0.5*lenInput) )   
 
-""" Plot input signal as a function of time """ # Do not uncomment unless time is <100 s, will overload with data...
-#plt.plot( time_s, input )
-#plt.show()
+    
+    """ Plot frequencies in signal """
+    plt.plot( freq, np.abs( fftOutput[:halfLenInput] ) ) # Take first half of FFT, take abs value
+    plt.grid()
+    plt.show()
+    
+    """ Plot input signal as a function of time """ # Do not uncomment unless time is <100 s, will overload with data...
+    #plt.plot( time_s, input )
+    #plt.show()
+
+
+# Run the functions; compute and look at output
+#--------------------------------------------------
+inputData = dataInitialise(lenInput, sampleRate)
+fftOutput = fft( inputData )
+plotGraphs( fftOutput, sampleRate )
+
+
+
+
+    
+
+
+
+
+
