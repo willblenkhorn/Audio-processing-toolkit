c8fc57b58bf9ec0f8d2df195e91349dfc05f4abe
joshring <frag180@gmail.com>
2016-05-01 23:29:28 +0100

    Update pyfftw_test.py

--- pyfftw_test.py
+++ pyfftw_test.py
@@ -11,12 +11,13 @@ import pyfftw   # FFTW library python bindings
 import multiprocessing # Gets num cpu threads
 import matplotlib.pyplot as plt # graph plotting
 
+
 nthreads = multiprocessing.cpu_count()
 sampleRate = 41000 # Hz
-tInput = 0.1 # Seconds
+tInput = 0.02 # Seconds
 lenInput = tInput * sampleRate
-freq1 = 200 # Hz  
-freq2 = 7000 # Hz
+freq1 = 2000 # Hz  
+freq2 = 500 # Hz
 nyquist = 2*sampleRate # Hz, this is the max freq, can reliably be sampled
 time_s = np.linspace( 0.0, (lenInput/sampleRate),  num = lenInput ) # Time for cosine functions
 
@@ -27,9 +28,11 @@ def dataInitialise(lenInput, sampleRate):
     Returns fft test input data as a numpy array
     """
     start = time.time() # start timer
-    
-    inputData = 2*np.random.rand(lenInput).astype('complex128') # 64 bit real and 64 bit imag, further 4x speed if use 32bit
-    inputData += np.cos( 2*np.pi*time_s * freq1 ) + np.cos( 2*np.pi*time_s * freq2 )
+    #inputData = np.zeros(lenInput).astype('complex128') # To initialise the data without noise uncomment this
+    # Noise
+    inputData = 1*np.random.rand(lenInput).astype('complex128') # 64 bit real and 64 bit imag, further 4x speed if use 32bit
+    # Signal frequencies
+    inputData += 1*np.cos( 2*np.pi*time_s * freq1 ) + 1*np.cos( 2*np.pi*time_s * freq2 )
     dataAverage = np.average( inputData )
     inputData -= dataAverage # Detrending the input data so it is zero centred
     
@@ -38,67 +41,88 @@ def dataInitialise(lenInput, sampleRate):
     return inputData 
 
 
-def fft( inputData ):
+def fft( fftInput ):
     """
-    Takes inputData as numpy array
+    Takes fftInput as numpy array
     Returns forward FFT calculated by FFTW
     """
-    pyfftw.forget_wisdom() # Detail of FFTW
+    pyfftw.forget_wisdom() # Detail of FFTW needed to stop it messing up
     start = time.time()
-    fftOutput = np.empty_like(inputData)
+    fftOutput = np.empty_like( fftInput )
     # Setting up and running FFT (forward) calculation
-    fft = pyfftw.FFTW( inputData, fftOutput, direction='FFTW_FORWARD', flags=('FFTW_MEASURE', ), threads=nthreads, planning_timelimit=None )
+    fft = pyfftw.FFTW( fftInput, fftOutput, direction='FFTW_FORWARD', flags=('FFTW_MEASURE', ), threads=nthreads, planning_timelimit=None )
     fft()
     
     timetaken = time.time() - start
     print('Time taken for FFT with %d cores is %.3f secs.' % (nthreads, timetaken))
-    fftOutput = (1/ (0.5*len(fftOutput)) ) * fftOutput # FFT normalisation
+    fftOutput *= (1/ (0.5*len(fftOutput)) ) # FFT normalisation
     return fftOutput
+    
+
+def primativeFilter( filterInput ):
+    """
+    Very simple filter which just zeros frequencies
+        You must use np.copy to avoid simply passing a reference
+    """
+    filterOutput = np.copy( filterInput )
+    bandwidth = int( len(filterOutput)*0.5*0.95 )
+##
+    filterOutput[ (len(filterOutput)*0.5-bandwidth) : (len(filterOutput)*0.5+bandwidth) ] = 0.+0.J
+    return filterOutput
+    
+    
+def ifft( ifftInput ):
+    """
+    Takes ifftInput as numpy array
+    Returns backward (reverse) FFT calculated by FFTW
+    """
+    pyfftw.forget_wisdom() # Detail of FFTW needed to stop it messing up
+    start = time.time()
+    
+    
+    ifftOutput = np.empty_like( ifftInput )
+    # Setting up and running FFT (Backward) calculation
+    fft = pyfftw.FFTW( ifftInput, ifftOutput, direction='FFTW_BACKWARD', flags=('FFTW_MEASURE', ), threads=nthreads, planning_timelimit=None )
+    fft()
+    
+    timetaken = time.time() - start
+    print('Time taken for FFT with %d cores is %.3f secs.' % (nthreads, timetaken))
+    ifftOutput *=  0.5*len(ifftOutput) # FFT normalisation
+   
+    
+    return ifftOutput
 
 
-def plotGraphs( fftOutput, sampleRate ):
+def plotGraphs( originalData, originalFFT, filteredData, filteredFFT, samplingRate ):
     """
     Use matplotlib to plot graphs
-    
     TODO: Not currently set up to handle odd numbered lengths of input
     """
-    lenInput = int( len(fftOutput) )
-    halfLenInput = int( lenInput/2 ) # Used for graphing 
-
+    halfLength = int( lenInput/2 ) # Used for graphing, needs ints
     # Frequencies from 0 to 1/2 sampling freq, number of elements = 1/2 total, due to FFT symmetry
-    freq = np.linspace( 0.0, (0.5*sampleRate), (0.5*lenInput) )   
+    freqAxis = np.linspace( 0.0, (0.5*samplingRate), (0.5*lenInput) )   
 
-    
-    """ Plot frequencies in signal """
-    plt.subplot(2, 1, 1)
-    plt.plot( freq, np.abs( fftOutput[:halfLenInput] ) ) # Take first half of FFT, take abs value
-    plt.title('Frequency and time data')
-    plt.ylabel('Amplitude a.u.')
-    plt.xlabel('Frequency / Hz')
-    plt.grid()
-    
-    """ Plot input signal as a function of time """ 
-    plt.subplot(2, 1, 2)
-    plt.plot( time_s, inputData )
-    plt.xlabel('time / s')
-    plt.ylabel('Intensity')
-    plt.grid()
+    f, axarr = plt.subplots(2, 2)
+    axarr[0, 0].set_title('Signal orig.') 
+    axarr[0, 0].plot( time_s, originalData )
+    axarr[1, 0].set_title('FFT original')
+    axarr[1, 0].plot( freqAxis, np.abs( originalFFT[:halfLength] ) )
+    axarr[0, 1].set_title('Signal filt.')
+    axarr[0, 1].plot( time_s, filteredData )
+    axarr[1, 1].set_title('FFT filt')
+    axarr[1, 1].plot( freqAxis, np.abs( filteredFFT[:halfLength] ) )
     
     plt.show()
-    
 
 # Run the functions; compute and look at output
 #--------------------------------------------------
-inputData = dataInitialise(lenInput, sampleRate)
-fftOutput = fft( inputData )
-plotGraphs( fftOutput, sampleRate )
+data = dataInitialise(lenInput, sampleRate)
+dataFFT = fft( data )
+dataFFTfiltered = primativeFilter( dataFFT )
+dataFiltered = ifft( dataFFTfiltered )
 
 
-
-
-    
-
-
+plotGraphs( data, dataFFT, dataFiltered, dataFFTfiltered, sampleRate )
 
 
 
